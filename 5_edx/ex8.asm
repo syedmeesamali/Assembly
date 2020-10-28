f8a0f36f0311f5ed500382c0e6a9768cba130361
syedmeesamali <syedmeesamali@yahoo.com>
2020-10-28 18:21:21 +0400

    System return calls

--- 5_edx/ex8.asm
+++ 5_edx/ex8.asm
@@ -2,8 +2,20 @@ global _start
 _start:
 	call func
 	mov eax, 1
+	mov ebx, 0
 	int 0x80
+
 func:
-	mov ebx, 42
-	pop eax			;pop-off the location from stack to eax
-	jmp eax
+	mov ebp, esp		;Save to base-pointer location of stack-pointer
+	sub esp, 2			;reserve 2 bytes from stack
+	mov [esp], byte 'H'
+	mov [esp+1], byte 'i'
+	mov eax, 4		;system_write call
+	mov ebx, 1		;stdout file descriptor
+	mov ecx, esp	;location of bytes to write
+	mov edx, 2		;number of bytes to write
+	int 0x80		;perform the system call
+	mov esp, ebp	;restore the stack pointer condition
+	ret
+	
+
