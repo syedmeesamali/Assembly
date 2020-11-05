7513319145473596d09d7c01e2944ab5d8740fe2
syedmeesamali <syedmeesamali@yahoo.com>
2020-11-05 15:26:58 +0400

    Sum working only less than 10

--- 6_tutor/first.asm
+++ 6_tutor/first.asm
@@ -27,3 +27,49 @@ main:				;entry point for linker
 	mov ecx, msg1
 	mov edx, len1
 	int 0x80
+
+	mov eax, SYS_READ
+	mov ebx, STDIN
+	mov ecx, num1
+	mov edx, 2
+	int 0x80
+
+	mov eax, SYS_WRITE
+	mov ebx, STDOUT
+	mov ecx, msg2
+	mov edx, len2
+	int 0x80
+
+	mov eax, SYS_READ
+	mov ebx, STDIN
+	mov ecx, num2
+	mov edx, 2
+	int 0x80
+
+	mov eax, SYS_WRITE
+	mov ebx, STDOUT
+	mov ecx, msg3
+	mov edx, len3
+	int 0x80
+
+	;move first num to eax and 2nd to ebx and subtract 0 to make it decimal
+
+	mov eax, [num1]
+	sub eax, '0'
+	mov ebx, [num2]
+	sub eax, '0'
+	
+	;add eax and ebx
+	add eax, ebx
+	add eax, '0'		;convert result to decimal
+	mov [res], eax		;more result to memory location with name res
+	
+	mov eax, SYS_WRITE
+	mov ebx, STDOUT
+	mov ecx, res
+	mov edx, 1
+	int 0x80
+exit:
+	mov eax, SYS_EXIT
+	xor ebx, ebx
+	int 0x80
