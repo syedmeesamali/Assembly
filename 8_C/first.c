c771b0af56ac8b267be88f9f8ba657b5a4de3a22
syedmeesamali <syedmeesamali@yahoo.com>
2021-09-24 23:03:34 +0330

    switch in C - working on rpi 3b+ only via gcc

--- 8_C/first.c
+++ 8_C/first.c
@@ -2,6 +2,17 @@
 int main()
 {
  int a = 0;
- printf("A is %i", a);
+ int b = 0;
+ int state = 0;
+ printf("enter state of b\n");
+ scanf("%d", &b);
+ switch (b) {
+ case (0): if (b) state = 0; else state = 1; break;
+ case (1): state = 2; break;
+ case (2): if (b==2) state = 3; else state = 4; break;
+ default: state = 5;
+ }
+ printf("state is %d", state);
+ printf("\n b is %d", b);
  return 0;
 }
