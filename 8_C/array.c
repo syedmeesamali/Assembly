bc98ebe58fbc2f3110949813a6e9cf9387d8996d
syedmeesamali <syedmeesamali@yahoo.com>
2021-10-04 23:24:18 +0330

    pointers review from new course edx

--- 8_C/array.c
+++ 8_C/array.c
@@ -7,16 +7,26 @@ typedef struct point {
 } point;
 
 
+//Person as a structure
+typedef struct person {
+	char name[80];
+	float height;
+	int age;
+} person;
+
 void main(void)
 {
   	char name[80];
 	int len;
 	char c;
 	strcpy(name, "meesam");
+	person p2;
+	int s1 = sizeof(p2);
 	point p1;
 	p1.x = 42; p1.y = 50;
 	len = strlen(name);
 	c = name[2];
 	printf("len is %d and 3rd char is %c\n", len, c);
 	printf("For point p1 x val is %d and y val is %d\n", p1.x, p1.y);
+	printf("\n Size of person struct is %d", s1);
 }
---
+++ 8_C/pointer.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(void)
+{
+	char age = 30;
+	char *p;		//Pointer to age
+	p = &age;
+	printf("\n Age = %d \n", age);
+	printf("\n p = %p \n", p);
+	printf("\n *p = %p \n", *p);
+	printf("\n sizeof(age)  = %ld \n", sizeof(age));
+	printf("\n sizeof(p)  = %ld \n", sizeof(p));
+	*p = 40;
+	printf("\n *p  = %d \n", *p);
+	printf("\n age  = %d \n", age);
+	return 0;
+}
